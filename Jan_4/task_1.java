658588501c15c0b95adbfba9c1b350d1524a1642
Ansh0226 <ansh.114.ar@gmail.com>
2024-01-06 17:35:23 +0530

    4th jan potd completed

---
+++ Jan_4/task_1.java
@@ -0,0 +1,21 @@
+//Leetcode Q no. 14
+class task_1 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            while (j < prefix.length() && j < strs[i].length() && prefix.charAt(j) == strs[i].charAt(j)) {
+                j++;
+            }
+
+            if (j == 0) {
+                return "";
+            }
+
+            prefix = prefix.substring(0, j);
+        }
